a30f0e74b336f6623444fab78c8a6b618b4fd20b
SniffBakaSniff <subaka186@gmail.com>
2023-11-04 20:35:10 -0700

    Made Tools Burn in Furnace!
    Textures Update!
    Thank You LieOnLion!!!

--- src/main/java/xyz/subaka/items/MoreToolsEnum.java
+++ src/main/java/xyz/subaka/items/MoreToolsEnum.java
@@ -5,19 +5,19 @@ import xyz.subaka.WoodenToolVariants;
 
 public enum MoreToolsEnum {
 
-    SPRUCE,
-    BIRCH,
-    JUNGLE,
-    ACACIA,
-    DARK_OAK,
-    MANGROVE,
-    CHERRY,
-    BAMBOO,
-    CRIMSON,
-    WARPED;
+    spruce,
+    birch,
+    jungle,
+    acacia,
+    dark_oak,
+    mangrove,
+    cherry,
+    bamboo,
+    crimson,
+    warped;
 
     public Identifier getId(){
-        return new Identifier(WoodenToolVariants.MOD_ID, this.name().toLowerCase());
+        return new Identifier(WoodenToolVariants.MOD_ID, this.name());
     }
 
 }
