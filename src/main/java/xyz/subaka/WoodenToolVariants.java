b75c7a1f692be4c07df61e47928e05d70a8efaac
SniffBakaSniff <subaka186@gmail.com>
2023-11-10 02:10:49 -0800

    Mod support and stuffs

--- src/main/java/xyz/subaka/WoodenToolVariants.java
+++ src/main/java/xyz/subaka/WoodenToolVariants.java
@@ -1,27 +1,14 @@
 package xyz.subaka;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.CommonLifecycleEvents;
-import net.fabricmc.fabric.api.registry.FuelRegistry;
-import net.minecraft.item.Item;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.util.Identifier;
 import xyz.subaka.items.ModItems;
 
 
 public class WoodenToolVariants implements ModInitializer {
 	public static final String MOD_ID = "woodentoolvariants";
-	public static final TagKey<Item> TOOL_IS_FUEL = TagKey.of(RegistryKeys.ITEM, new Identifier(WoodenToolVariants.MOD_ID, "tool_is_fuel"));
 
 	@Override
 	public void onInitialize() {
 		ModItems.register();
-
-
-		CommonLifecycleEvents.TAGS_LOADED.register((identifier, tag) -> {
-			FuelRegistry.INSTANCE.add(TOOL_IS_FUEL, 200);
-
-		});
 	}
 }
